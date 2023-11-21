bb7a51d342de1700f94fc3919a9abe89abc64056
socratic617 <erik.solis02135@gmail.com>
2023-11-21 11:49:04 -0500

    moved root pg on mvc

---
+++ middleware/cloudinary.js
@@ -0,0 +1,14 @@
+const cloudinary = require("cloudinary").v2;
+
+require("dotenv").config({ path: "./config/.env" });
+
+console.log(process.env.CLOUD_NAME)
+console.log(process.env.API_KEY)
+console.log(process.env.API_SECRET)
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+module.exports = cloudinary;
