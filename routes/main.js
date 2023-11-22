aa0ab7b7ee4eab9640eea74d4b7579204f51632b
socratic617 <erik.solis02135@gmail.com>
2023-11-22 00:45:24 -0500

    added routes for journal entry pg and community pg

--- routes/main.js
+++ routes/main.js
@@ -2,13 +2,16 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
-const postsController = require("../controllers/posts");
+const journalsController = require("../controllers/journals");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
 router.get("/", homeController.getIndex); // (endpoint, callback function)
-router.get("/profile", ensureAuth, postsController.getProfile);
-router.get("/feed", ensureAuth, postsController.getFeed);
+// router.get("/profile", ensureAuth, journalsController.getProfile);
+router.get("/community-page", ensureAuth, journalsController.getCommunity);//added commmunity pg 
+router.get("/journal-entry", ensureAuth, journalsController.getJournal);
+// router.get("/feed", ensureAuth, journalsController.getFeed);
+// router.get("/feed", ensureAuth, journalsController.getFeed);
 router.get("/login", authController.getLogin);
 router.post("/login", authController.postLogin);
 router.get("/logout", authController.logout);
