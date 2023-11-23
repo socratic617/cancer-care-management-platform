4ae5b819553df7cdffe014dd4aaa0dedad262040
socratic617 <erik.solis02135@gmail.com>
2023-11-23 03:04:38 -0500

    added put for fav post, post for img, delete entry

--- routes/journals.js
+++ routes/journals.js
@@ -4,6 +4,8 @@ const upload = require("../middleware/multer");
 const journalsController = require("../controllers/journals");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
+router.put("/updateFavorite", journalsController.updateFavorite);
+router.delete("/deleteJournal", journalsController.deleteJournal);
 router.post("/journal-entry", upload.single("file"), journalsController.postJournal);
-
+router.post("/", upload.single("file"), journalsController.postJournal);
 module.exports = router;
