3a6875042f99407ac5e1f6d912191042f72ecc9a
socratic617 <erik.solis02135@gmail.com>
2023-11-24 03:43:12 -0500

    add comments

--- routes/journals.js
+++ routes/journals.js
@@ -4,6 +4,8 @@ const upload = require("../middleware/multer");
 const journalsController = require("../controllers/journals");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
+//.getVisualization is my key that gets me my value (located in journalsController and that value is a method aka async anonymous function)
+// router.get("/getVisualization", journalsController.getVisualization);//my router.get() method gets my callback function from key getVisualization but if it wasnt  router.get and you removed that, it would be a regular function
 router.put("/updateFavorite", journalsController.updateFavorite);
 router.delete("/deleteJournal", journalsController.deleteJournal);
 router.post("/journal-entry", upload.single("file"), journalsController.postJournal);
