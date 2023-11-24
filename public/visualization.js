30db5109d9100480bb31b16fe3fa8884a100fd79
socratic617 <erik.solis02135@gmail.com>
2023-11-24 11:56:49 -0500

    clean up code

--- public/visualization.js
+++ public/visualization.js
@@ -2,47 +2,6 @@
 const serverJournalData = JSON.parse(serverData.replaceAll("&#34;", "\""));
 console.log(serverJournalData);
 
-
-// const labels = [1,2,3,4,5,6,7]
-// const data = {
-//   labels: labels,
-//   datasets: [{
-//     label: 'My First Dataset',
-//     data: [65, 59, 80, 81, 56, 55, 40],
-//     backgroundColor: [
-//       'rgba(255, 99, 132, 0.2)',
-//       'rgba(255, 159, 64, 0.2)',
-//       'rgba(255, 205, 86, 0.2)',
-//       'rgba(75, 192, 192, 0.2)',
-//       'rgba(54, 162, 235, 0.2)',
-//       'rgba(153, 102, 255, 0.2)',
-//       'rgba(201, 203, 207, 0.2)'
-//     ],
-//     borderColor: [
-//       'rgb(255, 99, 132)',
-//       'rgb(255, 159, 64)',
-//       'rgb(255, 205, 86)',
-//       'rgb(75, 192, 192)',
-//       'rgb(54, 162, 235)',
-//       'rgb(153, 102, 255)',
-//       'rgb(201, 203, 207)'
-//     ],
-//     borderWidth: 1
-//   }]
-// };
-
-// const config = {
-//   type: 'bar',
-//   data: data,
-//   options: {
-//     scales: {
-//       y: {
-//         beginAtZero: true
-//       }
-//     }
-//   },
-// };
-
 //PURPOSE: to create an array of dates that represents my most recent days
 //credit: Chatgpt
 function getPastSevenDays() {
@@ -51,6 +10,7 @@ function getPastSevenDays() {
 
   //This allows me to loop through my 7 most recent days to create an array of dates with my data
   for (let i = 0; i < 14; i++) {
+
     const pastDate = new Date(today);
     pastDate.setDate(today.getDate() - i);
 
@@ -73,12 +33,14 @@ let resultSevenDaysMood = {}
 let resultSevenDaysHydration = {}
 let resultSevenDaysFatigue = {}
 let resultSevenDaysProtein = {}
+
 //this is to loop through my recent 7 days for mood/fluidIntake/ProteinIntake
 for(let i = 0; i < pastSevenDaysLabels.length; i++){
   console.log("pastSevenDaysLabels[i] (inside for loop) :")
   console.log(pastSevenDaysLabels[i])
 
   let found = false;
+
   //looping through the dates of past 7 days to check for existing entry for mood/protein/fluidIntake
   for(const key in serverJournalData ){
     let dateA = new Date(pastSevenDaysLabels[i])
@@ -90,7 +52,9 @@ for(let i = 0; i < pastSevenDaysLabels.length; i++){
       console.log( 'dateB')
       console.log(dateB)
       found = true;
-      if(resultSevenDaysMood['none'] == undefined ){// if this label doesnt exist, create it 
+
+      // if this label doesnt exist, create it 
+      if(resultSevenDaysMood['none'] == undefined ){
         resultSevenDaysMood['none'] = 1 
       } else { // if no label none, make the value = 1
         resultSevenDaysMood['none'] +=1
@@ -112,7 +76,9 @@ for(let i = 0; i < pastSevenDaysLabels.length; i++){
       console.log('serverJournalData[key]fatigueEntry  :')
       console.log(serverJournalData[key].fatigueEntry)
       found = true;
-      if(resultSevenDaysMood[serverJournalData[key].mood] == undefined ){// if this label doesnt exist, create it 
+
+      // if this label doesnt exist, create it 
+      if(resultSevenDaysMood[serverJournalData[key].mood] == undefined ){
         resultSevenDaysMood[serverJournalData[key].mood] = 1 
       } else { // if no label none, make the value = 1
         resultSevenDaysMood[serverJournalData[key].mood] +=1
@@ -123,7 +89,9 @@ for(let i = 0; i < pastSevenDaysLabels.length; i++){
   }
 
   if(!found){
-    if(resultSevenDaysMood['none'] == undefined ){// if this label doesnt exist, create it 
+    
+    // if this label doesnt exist, create it 
+    if(resultSevenDaysMood['none'] == undefined ){
       resultSevenDaysMood['none'] = 1 
     } else { // if no label none, make the value = 1
       resultSevenDaysMood['none'] +=1
@@ -148,10 +116,13 @@ const data = {
     label: 'mood',
     data: valuesArray,// valueArray holds my value for keyArrays
     backgroundColor: [
-      'rgb(255, 99, 132)',
-      'rgb(54, 162, 235)',
-      'rgb(255, 205, 86)',
-      'rgb(86,44,128)'
+      'rgb(86,44,128)',
+      'rgb(221,160,221)',
+      'rgb(69,44,99)',
+      'rgb(216,191,216)',
+      'rgb(230,230,250)',
+      'rgb(186,88,168)',
+      'rgb(186,88,168)'
     ],
     hoverOffset: 35
   }]
@@ -169,7 +140,7 @@ const config = {
    }
 };
 new Chart(
-    document.getElementById('visual-sleep'),
+    document.getElementById('visual-mood'),
     config
 );
 
