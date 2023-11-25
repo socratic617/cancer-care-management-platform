70050d074cd2bf5fe3be0fdf0583f7c536f9303c
socratic617 <erik.solis02135@gmail.com>
2023-11-25 03:57:39 -0500

    add search and see more feature

--- public/community.js
+++ public/community.js
@@ -1,5 +1,6 @@
 var trash = document.getElementsByClassName("trash");
 var favorite = document.getElementsByClassName("favorite")
+var seeMore = document.querySelectorAll('[id^="dots-"]');
 
 Array.from(favorite).forEach(function(element) {
   console.log("adding favorite listener too : ", element);
@@ -47,18 +48,55 @@ Array.from(trash).forEach(function(element) {
 });
 
 
+document.addEventListener('DOMContentLoaded', () => {
+    const input = document.getElementById('simple-search');
+    input.addEventListener('input', search);
+})
 
-// var communityCard = document.getElementsByClassName("see-more")
+// Credit: ChatGPT and https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_filter_list
+function search(e){
+  const inputValue = e.target.value.toUpperCase();
 
-// function toggleText(e) {
-//   var content = document.getElementById("content");
+  //Get all <p></p> elements with journal description information
+  const descriptions = document.querySelectorAll('[id^="journal-description-"]');
 
-  // if (content.style.maxHeight) {
-  //   content.style.maxHeight = null;
-  // } else {
-  //   content.style.maxHeight = content.scrollHeight + "px";
-  // }
-// }
+  console.log('descriptions')
+  console.log(descriptions)
+
+  //go through all of the elements
+  for(let description of descriptions){
+
+    //get the parent div aka card that holds the journal
+    let card = document.querySelector('.card-'+ description.id.replace('journal-description-', ''));
+
+    //check if the searched input exists, if not hide it
+    if (description.innerText.toUpperCase().indexOf(inputValue) < 0) {
+        card.style.display = "none"
+        console.log("found a match")
+        console.log(card)
+      } else {
+        card.style.display = ""
+        console.log("does not match")
+      }
+
+  }
+
+}
+
+Array.from(seeMore).forEach(function(element) {
+  console.log("adding delete listner too : ", element);
+
+    element.addEventListener('click', expandText);
+});
+
+//credit: https://www.w3schools.com/howto/howto_js_read_more.asp
+function expandText(e) {
+
+  let seeMoreSpan = document.querySelector('#more-'+ e.target.id.replace('dots-', ''));
+  e.target.style.display = "none";
+  seeMoreSpan.style.display = "inline";
+}
+
 
 
 
