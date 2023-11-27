9c9845fa041b12828ae1e46c365257f570077939
socratic617 <erik.solis02135@gmail.com>
2023-11-27 08:13:27 -0500

    add logic for most liked journal

--- controllers/journals.js
+++ controllers/journals.js
@@ -11,17 +11,54 @@ const newConfig = new Configuration({//SETTING UP OPEN AI
 });
 const openai = new OpenAIApi(newConfig);
 
+//convert Date to mm/dd/yyyy
+function formatDate(date) {
+  const month = (date.getMonth() + 1).toString().padStart(2, '0');
+  const day = date.getDate().toString().padStart(2, '0');
+  const year = date.getFullYear().toString();
+
+  return `${month}/${day}/${year}`;
+}
+
+
 module.exports = {
 
   getCommunity: async (req, res) => {//getCommunity is my method for journalsController 
-    try {// is going to run the code and if error, itll exit my try go to my catch error
+    try {// is going to run the code and if error, itll exit my try and  go to my catch error
       console.log('req.user : ')
       console.log(req.user)
+
+      //These two get the current date and the date from 7 days ago
+      const currentDate = new Date();
+      const sevenDaysAgo = new Date(currentDate);
+      sevenDaysAgo.setDate(currentDate.getDate() - 7);
+
+      //gets me the past 7 days of journals from my MONGO DB
+      const pastSevenDaysJournals = await Journal.find({
+        entryDate: {//trying to find my past 7 day journals that dont have private mode
+          $gte: formatDate(sevenDaysAgo),
+          $lt: formatDate(currentDate)
+        },
+        privateMode: false //preventing display of highlighted post for private journals 
+      });
+
+
+      //Goal: Get the journal with the most # of reactions.
+      let mostLikedJournal = pastSevenDaysJournals[0]
+      for (let i = 1; i < pastSevenDaysJournals.length; i++){
+        let reactionCountLiked = Object.keys(mostLikedJournal.reactions).length
+        let reactionCountSevenDays = Object.keys(pastSevenDaysJournals[i].reactions).length;
+  
+        if (reactionCountSevenDays > reactionCountLiked){
+          mostLikedJournal = pastSevenDaysJournals[i]
+        }
+      }
 
       //sort journal entries and hide ones where private mode is on
       const journals = await Journal.find({ privateMode: false }).sort({ entryDate: "desc" }).lean();// go and find me the journals (entries that users inputted )
 
-      res.render("community-page.ejs", { user: req.user, journals: journals });//"journals : journals" input where your entries[] is .  respond with creating my 'community-page.ejs'
+      /**"journals : journals" input where your entries[] are and mostliked journal is my highest reacted journal in the past seven day .  respond with creating my 'community-page.ejs'*/
+      res.render("community-page.ejs", { user: req.user, journals: journals, mostLikedJournal: mostLikedJournal });
 
     } catch (err) {
       console.log(err);
@@ -131,13 +168,15 @@ module.exports = {
 
 
       // go through all my keys in my req.body object  req = {body: {},parms:{}}
+      //REFORMATING HEALTH BIOMETRICS SECTION INTO SOMETHING CLEANER WHICH IS AN ARRAY OF OBJECTS WHERE EACH OBJECT REPRESENTS A ROW IN THE HEALTH BIOMETRICS
       for (const key in req.body) {
 
         console.log("------------key: ", key)
-        //entry-type-0 -> ['entry', 'type','0']
+        //entry-type-0 -> ['entry', 'type','0'] trying to seperate out index
         let keyArr = key.split('-');
 
-        /* checking for edge case if nothing inside object entry then stop and skip to the next key  in the loop */
+        /* checking for edge case if nothing inside object entry then stop and skip to the next key  in the loop 
+        keeping track where my new row is (when a entry or time shows up thats when i know its a new row*/
         if ((key.startsWith('time-') || (key == 'entry-date')) && Object.keys(entry).length !== 0) {
           console.log("IF: ", key)
 
@@ -159,7 +198,7 @@ module.exports = {
         }
       }
 
-      // Total sleep, Total hydration, totalProtein, totalActivity
+      //CREATING  Total sleep, Total hydration, totalProtein, totalActivity
       for(let i = 0; i < result.entries.length; i++) {
         let log = result.entries[i]
 
@@ -219,25 +258,28 @@ module.exports = {
       console.log('update favorite: req.user : ')
       console.log(req.user)
 
-      Journal.find({ _id: ObjectId(req.body.id) })
-        .exec((err, result) => {
+      Journal.find({ _id: ObjectId(req.body.id) })// this is trying to find the id of the journal that the user selected when they select the heart icon
+        .exec((err, result) => {// this is like a .then do this
           if (err) return console.log(err)
           console.log("result : ", result)
 
           console.log(result[0].reactions[req.body.loggedInUserId])
 
-          if (result[0].reactions[req.body.loggedInUserId] !== undefined) {
+          console.log(result[0].reactions)
+
+          //req.body.loggedeinuserid is my logged in user
+          // if its ture or false in reactions i want to remove user id when they unlike the post
+          if(result[0].reactions[req.body.loggedInUserId] !== undefined) {
             console.log('i need to remove the user id  ')
             delete result[0].reactions[req.body.loggedInUserId]; //credit: https://stackoverflow.com/questions/3455405/how-do-i-remove-a-key-from-a-javascript-object
-            //im removing the star if I dont want to star it 
-
-          }
-          else {
+          } else { //this is when i add my user id to the journal to signal that they liked that journal
             console.log('i need to add user id  ')
             result[0].reactions[req.body.loggedInUserId] = true; //this is how im adding star fav to post 
             console.log(result[0].reactions)
           }
 
+          // WHERE I UPDATE MY MONGO DB W / user id 
+          //update my reactions dpending whether they liked or unliked the journal 
           Journal.findOneAndUpdate({ _id: ObjectId(req.body.id) }, {
             $set: {
               reactions: result[0].reactions,
@@ -262,7 +304,7 @@ module.exports = {
 
       Journal.findOneAndDelete({ _id: ObjectId(req.body.id) }, (err, result) => {
         if (err) return res.send(500, err)
-        res.send('Recipe deleted!')
+        res.send('journal deleted!')
       })
     } catch (err) {
       console.log(err);
